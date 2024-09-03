ade12a69cd2c08bfe91ec89cc577f559df0619b0
mkrajina4 <mkrajina4@LAP-OZI-KRAJINA>
2024-09-03 16:39:27 +0200

    Add project files.

---
+++ Services/MovieService.cs
@@ -0,0 +1,27 @@
+﻿using CinemaApp.Models;
+
+namespace CinemaApp.Services
+{
+    public class MovieService
+    {
+        private readonly AppDbContext _context;
+        public MovieService(AppDbContext context) { _context = context; }
+
+        public double GetPrice (string name)
+        {
+            return _context.Movies.FirstOrDefault(s => s.Title == name).Price;
+        }
+
+        public int GetId (string name)
+        {
+            return _context.Movies.FirstOrDefault(s => s.Title == name).Id;
+        }
+
+        public MovieModel GetMovieModel(string name) 
+        {
+            return _context.Movies.FirstOrDefault(s => s.Title == name);
+        }
+
+
+    }
+}
